8b96f43de26014457a4445e77860686113f2acf0
Ubuntu <ubuntu@ip-172-31-17-81.ec2.internal>
2019-09-13 20:51:07 +0000

    added program

---
+++ rectangle_calculator.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2019 Liam Hearty All rights reserved.
+//
+// Created by: Liam Hearty
+// Created on: September 2019
+// This program will calculate the Area and Perimeter of a 3mm by 5mm rectangle
+
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions: 3mm x 5mm" << std::endl;
+    std::cout << "What are the area/perimeter of the rectangle?" << std::endl;
+    std::cout << "Area=" << (3*5) << std::endl;
+    std::cout << "Perimeter=" << (2*(3+5)) << std::endl;
+}
